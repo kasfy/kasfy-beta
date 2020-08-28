ae7c4d51d653b811d671fe5e0c907f9cb7e5a60f
Katheesh <ikatheesh@gmail.com>
2020-08-28 11:29:22 +0530

    changed es6 style

--- routes/web.js
+++ routes/web.js
@@ -1,44 +1,49 @@
-import { action, get } from "../config/routes";
+/* The NodeJS Framework for Smart Back-End
+   ▄█   ▄█▄    ▄████████    ▄████████    ▄████████ ▄██   ▄   
+  ███ ▄███▀   ███    ███   ███    ███   ███    ███ ███   ██▄ 
+  ███▐██▀     ███    ███   ███    █▀    ███    █▀  ███▄▄▄███ 
+ ▄█████▀      ███    ███   ███         ▄███▄▄▄     ▀▀▀▀▀▀███ 
+▀▀█████▄    ▀███████████ ▀███████████ ▀▀███▀▀▀     ▄██   ███ 
+  ███▐██▄     ███    ███          ███   ███        ███   ███ 
+  ███ ▀███▄   ███    ███    ▄█    ███   ███        ███   ███ 
+  ███   ▀█▀   ███    █▀   ▄████████▀    ███         ▀█████▀  
+  ▀ Author : S.Katheeskumar [https://katheesh.github.io] */
+
+import { action, get, post, put } from "../config/routes";
 import HomeController from "../app/controllers/HomeController";
 
 export default class WebController {
 
-  	constructor() {
-    	this.home = new HomeController();
-  	}
+	constructor() {
+		this.home = new HomeController();
+	}
 
-  	@get("/")
-  	welcome(req, res) {
-  		this.home.welcome(req, res);
-  	}
+	@get("/")
+	welcome(req, res) {
+		this.home.welcome(req, res);
+	}
 
-  	@get("/login")
-  	Login(req, res) {
-    	res.json({ msg: "login" });
-  	}
+	@get("/login")
+	Login(req, res) {
+		res.json({
+			msg: "login"
+		});
+	}
 
-  	@get("/register")
-  	Register(req, res) {
-    	res.json({ msg: "Register" });
-  	}
+	@get("/register")
+	Register(req, res) {
+		res.json({
+			msg: "Register"
+		});
+	}
 
-  	@get("/home")
-  	Home(req, res) {
-    	//res.json({ msg: this.home.home() });
-    	res.render(this.home.home());
-  	}
+	@post("/login")
+	Login(req, res) {
+
+	}
+
+	@post("/register")
+	Register(req, res) {
+
+	}
 }
-
-/* The NodeJS Framework for Smart Back-End
- .----------------.  .----------------.  .----------------.  .----------------.  .----------------. 
-| .--------------. || .--------------. || .--------------. || .--------------. || .--------------. |
-| |  ___  ____   | || |      __      | || |    _______   | || |  _________   | || |  ____  ____  | |
-| | |_  ||_  _|  | || |     /  \     | || |   /  ___  |  | || | |_   ___  |  | || | |_  _||_  _| | |
-| |   | |_/ /    | || |    / /\ \    | || |  |  (__ \_|  | || |   | |_  \_|  | || |   \ \  / /   | |
-| |   |  __'.    | || |   / ____ \   | || |   '.___`-.   | || |   |  _|      | || |    \ \/ /    | |
-| |  _| |  \ \_  | || | _/ /    \ \_ | || |  |`\____) |  | || |  _| |_       | || |    _|  |_    | |
-| | |____||____| | || ||____|  |____|| || |  |_______.'  | || | |_____|      | || |   |______|   | |
-| |              | || |              | || |              | || |              | || |              | |
-| '--------------' || '--------------' || '--------------' || '--------------' || '--------------' |
- '----------------'  '----------------'  '----------------'  '----------------'  '----------------' 
-Author : S.Katheeskumar [https://katheesh.github.io]*/
