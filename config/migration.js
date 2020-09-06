3e0423834f6ae2699c0b2b0ba5219387abf522ae
Katheesh <katheeskumar@outlook.com>
2020-09-06 09:42:19 +0530

    initial start work for migration

---
+++ config/migration.js
@@ -0,0 +1,41 @@
+/* The NodeJS Framework for Smart Back-End
+   ▄█   ▄█▄    ▄████████    ▄████████    ▄████████ ▄██   ▄   
+  ███ ▄███▀   ███    ███   ███    ███   ███    ███ ███   ██▄ 
+  ███▐██▀     ███    ███   ███    █▀    ███    █▀  ███▄▄▄███ 
+ ▄█████▀      ███    ███   ███         ▄███▄▄▄     ▀▀▀▀▀▀███ 
+▀▀█████▄    ▀███████████ ▀███████████ ▀▀███▀▀▀     ▄██   ███ 
+  ███▐██▄     ███    ███          ███   ███        ███   ███ 
+  ███ ▀███▄   ███    ███    ▄█    ███   ███        ███   ███ 
+  ███   ▀█▀   ███    █▀   ▄████████▀    ███         ▀█████▀  
+  ▀ Author : S.Katheeskumar [https://katheesh.github.io] */
+
+var fs = require('fs');
+
+function readFiles(dirname, onFileContent, onError) {
+  fs.readdir(dirname, function(err, filenames) {
+    if (err) {
+      onError(err);
+      return;
+    }
+    filenames.forEach(function(filename) {
+      fs.readFile(dirname + filename, 'utf-8', function(err, content) {
+        if (err) {
+          onError(err);
+          return;
+        }
+        onFileContent(filename, content);
+      });
+    });
+  });
+}
+
+var pathx = __dirname.substring(0, __dirname.length - 6) + "database/migrations/";
+
+console.log(pathx);
+var data = {};
+readFiles(pathx, function(filename, content) {
+  data[filename] = content;
+  console.log(data);
+}, function(err) {
+  throw err;
+});
